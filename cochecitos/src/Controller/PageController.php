6c0964a54594a1d165257eee11bf9b1605db3351
alejandroforcada <alejandroforcadacs@gmail.com>
2023-10-31 11:22:32 +0100

    Page controller actualizado

--- cochecitos/src/Controller/PageController.php
+++ cochecitos/src/Controller/PageController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Coche;
 
 class PageController extends AbstractController
 {
@@ -15,4 +17,13 @@ class PageController extends AbstractController
             'controller_name' => 'PageController',
         ]);
     }
+    #[Route('/', name:'inicio')]
+    public function buscar(ManagerRegistry $doctrine ):Response
+        {
+            $repositorio=$doctrine->getRepository(Coche::class);
+            $coches=$repositorio->findAll();
+            return $this -> render ('lista_coches.html.twig', [
+                'coches' => $coches
+            ]);
+        }
 }
